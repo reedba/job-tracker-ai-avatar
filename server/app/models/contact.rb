b1de126a4a1edb8e38392dc2925418825e80307e
reedba <rdsfence@gmail.com>
2025-10-29 12:20:14 -0400

    contact email unique validation

--- server/app/models/contact.rb
+++ server/app/models/contact.rb
@@ -3,6 +3,6 @@ class Contact < ApplicationRecord
   
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, allow_blank: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, allow_blank: true, uniqueness: { case_sensitive: false }
   validates :company_id, presence: true
 end
